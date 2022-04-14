7615489607eafcb913941488cdd40f90a3d85c41
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-04-14 17:41:42 +0530

    everything added

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Authentication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
