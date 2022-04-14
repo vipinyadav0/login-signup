75bed42b50e334df02adff466e2fa75744f977df
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-04-14 17:40:53 +0530

    urls routing modified

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('' , views.home),
+    path('' , views.home, name="home"),
+    path('logout', views.logout, name='logout'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
 ]
